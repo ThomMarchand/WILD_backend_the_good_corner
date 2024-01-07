96dfa90fa605cb0fe3955476b030e26f7b786ac8
ElF4TboY <thomasmarchand63@gmail.com>
2024-01-07 13:41:30 +0100

    :sparkles: API works with grphql

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,17 @@
 import { DataSource } from "typeorm";
 
-export default new DataSource({
+const db = new DataSource({
   type: "sqlite",
   database: "the_good_corner.sqlite",
   entities: ["src/entities/*.ts"],
   synchronize: true,
 });
+
+export const initializeDB = async (): Promise<void> => {
+  try {
+    await db.initialize();
+    console.log("Database successfully initialized");
+  } catch (e: any) {
+    console.log(`Database failed to connect ${e.message}`);
+  }
+};
